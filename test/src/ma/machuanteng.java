18a80989c83e8841a59ce3f4efed2d44fc962095
shallight <2985711841@qq.com>
2022-02-18 22:56:24 +0800

    Ma

--- test/src/ma/machuanteng.java
+++ test/src/ma/machuanteng.java
@@ -3,7 +3,7 @@ package ma;
 public class machuanteng {
     static public void method()
     {
-        System.out.println("machuanteng");
+        System.out.println("machuanteng is hungry now, but there is nothing to eat in the dormitory");
     }
 }
 
